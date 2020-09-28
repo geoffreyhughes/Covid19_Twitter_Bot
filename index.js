ac8c5631cb985303c27c590698f5433405472d19
Geoffrey Hughes <46589744+geoffreyhughes@users.noreply.github.com>
2020-09-27 19:13:01 -0700

    Got replies working

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var T = new Twit({
 
 // Read data into array of objects
 var loader = require('csv-load-sync');
-var csv_data = loader('/Users/geoffreyhughes/Projects/Covid19_bot/data/state_NYT_clean.csv');
+var csv_data = loader('/Users/geoffreyhughes/Projects/Covid19_Twitter_Bot/data/state_NYT_clean.csv');
 
 csv_data.forEach(state => console.log(state.date, ' ', state.state, ' has ', state.cases, ' cases and ', state.deaths, ' dead.',
 '#'.concat( state.state.replace(/\s/g, ''))));
@@ -26,9 +26,10 @@ console.log(date_today)
 
 // Head tweet, begins daily thread
 T.post('statuses/update', { status: '[' + date_today +
-"]: United States COVID-19 statistics by State. Data is cumulative and provided by @nytimes. Daily thread (alphabetical) #COVID19" }, function(err, data, response) {
-  console.log(data)
-  reply_tweet(data.id_str, 0)
+"]: United States COVID-19 statistics by State. Data is cumulative and provided by @nytimes. Daily thread. #COVID19" }, function(err, data, response) {
+  console.log(data);
+  console.log('Head tweet id_str: ', data.id_str);
+  reply_tweet(data.id_str, 0);
 })
 
 // As needed
@@ -38,20 +39,59 @@ T.post('statuses/update', { status: '[' + date_today +
 function reply_tweet(parent_id, curr_state) {
 
   var loader = require('csv-load-sync');
-  var csv_data = loader('/Users/geoffreyhughes/Projects/Covid19_bot/data/state_NYT_clean.csv');
+  var csv_data = loader('/Users/geoffreyhughes/Projects/Covid19_Twitter_Bot/data/state_NYT_clean.csv');
 
-  console.log(parent_id);
+  console.log('Current State index: ', curr_state, ' for ', csv_data[curr_state].state);
+  console.log('Tweeting in reply to ', parent_id);
   T.post('statuses/update', {in_reply_to_status_id: parent_id, auto_populate_reply_metadata: true,
-    status: '(' + csv_data[curr_state].state + '): ' + csv_data[curr_state].cases +
-    ' cases and ' + csv_data[curr_state].deaths + ' deaths. ' + '#'.concat( csv_data[curr_state].state.replace(/\s/g, '')) +
+    status: '(' + '#'.concat( csv_data[curr_state].state.replace(/\s/g, '')) + '): ' + csv_data[curr_state].cases +
+    ' cases and ' + csv_data[curr_state].deaths + ' deaths.' +
     ' @Covid19States'}, function(err, data, response) {
 
-      // Rows of list (states / territtories))
-      if (curr_state < 5) {
+      // Rows of list (states / territtories)
+      if (curr_state < 54) {
         curr_state += 1;
-        reply_tweet(data.id_str, curr_state)
+        console.log('calling fn with id_str (used): ', data.id_str);
+        console.log('calling fn with id (not used): ', data.id);
+        reply_tweet(data.id_str, curr_state);
       }
     })
 
-
 }
+
+// TODO: perhaps add an output file of each tweet's ID once it's sent
+// for easy deletion if necessary (and to just keep track)
+
+//
+//
+// T.post('statuses/update', { status: '6reply to me' }, function(err, data, response) {
+//   console.log(data);
+//   console.log('Head tweet id_str: ', data.id_str);
+//   reply_tweet(data.id_str, 0);
+// })
+//
+// // As needed
+// //reply_tweet(1310106912785006594, ... )
+//
+// // Recursive reply chain thread
+// function reply_tweet(parent_id, curr_state) {
+//
+//   var loader = require('csv-load-sync');
+//   var csv_data = loader('/Users/geoffreyhughes/Projects/Covid19_Twitter_Bot/data/state_NYT_clean.csv');
+//
+//   console.log('Current State index: ', curr_state, ' for ', csv_data[curr_state].state);
+//   console.log('Tweeting in reply to ', parent_id);
+//   T.post('statuses/update', {in_reply_to_status_id: parent_id,
+//     status: '6reply' + curr_state}, function(err, data, response) {
+//
+//       // Rows of list (states / territtories))
+//       if (curr_state < 2) {
+//         curr_state += 1;
+//         console.log('calling fn with id_str (used): ', data.id_str);
+//         console.log('calling fn with id (not used): ', data.id);
+//         reply_tweet(data.id_str, curr_state);
+//       }
+//     })
+//
+//
+// }
