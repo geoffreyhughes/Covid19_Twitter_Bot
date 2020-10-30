0135fed76c60636e00b0c8dce57873e519d6d34b
Geoffrey Hughes <46589744+geoffreyhughes@users.noreply.github.com>
2020-10-29 20:42:09 -0700

    starting moving graph

--- make_graphs.py
+++ make_graphs.py
@@ -50,5 +50,16 @@ def make_bar_graph(clean_file):
         print('+CREATED: ' + curr_dir + '/data/live/graphs/' + 'most_recent_bar_graph.png')
 
 
+def make_animated_line(clean_file):
+
+
+# color = ['red', 'green', 'blue', 'orange']
+# fig = plt.figure()
+# plt.xticks(rotation=45, ha="right", rotation_mode="anchor") #rotate the x-axis values
+# plt.subplots_adjust(bottom = 0.2, top = 0.9) #ensuring the dates (on the x-axis) fit in the screen
+# plt.ylabel('No of Deaths')
+# plt.xlabel('Dates')
+
+
 # # Percentage increase in cases
 # def
